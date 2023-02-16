a87016f509bbc64e3b5f6723b7922acbf158d036
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-16 21:40:02 +0100

    drf admin working

---
+++ frontend/src/secrets.exampleDocker.js
@@ -0,0 +1,7 @@
+const secrets = {
+    URL_DRF: "http://localhost/api/",
+    URL_GQL: "http://localhost/gql/",
+    MAP_TOKEN: "pk.eyJ1Ijoic2FudGlpbWFydGluZXoiLCJhIjoiY2t6eWZlYzk2MGIyOTJ2cDdxc2dmcDkxaSJ9.IhYesNObwvyMWu_nQQQoiw"
+}
+
+export default secrets;
