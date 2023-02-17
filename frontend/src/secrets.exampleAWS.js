64f887cd8c8a863bfe81cd0aa1a75466907d2551
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-17 18:50:22 +0100

    example secrets updated

--- frontend/src/secrets.exampleAWS.js
+++ frontend/src/secrets.exampleAWS.js
@@ -1,6 +1,6 @@
 const secrets = {
-    URL_DRF: "http://ec2-54-163-168-98.compute-1.amazonaws.com/api/",
-    URL_GQL: "http://ec2-54-163-168-98.compute-1.amazonaws.com/gql/",
+    URL_DRF: "https://ec2-54-163-168-98.compute-1.amazonaws.com/api/",
+    URL_GQL: "https://ec2-54-163-168-98.compute-1.amazonaws.com/gql/",
     MAP_TOKEN: "pk.eyJ1Ijoic2FudGlpbWFydGluZXoiLCJhIjoiY2t6eWZlYzk2MGIyOTJ2cDdxc2dmcDkxaSJ9.IhYesNObwvyMWu_nQQQoiw"
 }
 
