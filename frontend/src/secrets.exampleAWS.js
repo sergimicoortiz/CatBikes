339865f6d10aca099aed4b3ba5b92f4d892bb6f5
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-02-23 15:51:39 +0100

    nginx not using ssl

--- frontend/src/secrets.exampleAWS.js
+++ frontend/src/secrets.exampleAWS.js
@@ -1,6 +1,6 @@
 const secrets = {
-    URL_DRF: "https://40.76.235.242/api/",
-    URL_GQL: "https://40.76.235.242/gql/",
+    URL_DRF: "http://40.76.235.242/api/",
+    URL_GQL: "http://40.76.235.242/gql/",
     MAP_TOKEN:
         "pk.eyJ1Ijoic2FudGlpbWFydGluZXoiLCJhIjoiY2t6eWZlYzk2MGIyOTJ2cDdxc2dmcDkxaSJ9.IhYesNObwvyMWu_nQQQoiw",
 };
--- frontend/src/secrets.exampleDocker.js
+++ frontend/src/secrets.exampleDocker.js
@@ -1,6 +1,6 @@
 const secrets = {
-    URL_DRF: "https://localhost/api/",
-    URL_GQL: "https://localhost/gql/",
+    URL_DRF: "http://localhost/api/",
+    URL_GQL: "http://localhost/gql/",
     MAP_TOKEN:
         "pk.eyJ1Ijoic2FudGlpbWFydGluZXoiLCJhIjoiY2t6eWZlYzk2MGIyOTJ2cDdxc2dmcDkxaSJ9.IhYesNObwvyMWu_nQQQoiw",
 };
