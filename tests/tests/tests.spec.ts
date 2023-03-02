31d3ef0948c9ddfcb658fe723325078d79195476
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-03-02 20:29:07 +0100

    register test

--- tests/tests/tests.spec.ts
+++ tests/tests/tests.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 import dotenv from 'dotenv';
 dotenv.config();
 const URL = process.env.URL || 'http://localhost:3000/'
+const screenshotPath = './screenshots/';
 
 test('has title', async ({ page }) => {
   await page.goto(URL);
@@ -14,7 +16,6 @@ test('rent without being logged', async ({ page }) => {
   await page.locator('circle').nth(1).click();
   await page.getByRole('button', { name: 'DETAILS' }).click();
   await page.getByRole('button').first().click();
-  // await page.getByRole('button', { name: 'Rent Bike' }).first().click();
   await expect(page).toHaveURL(/login/);
 });
 
@@ -29,6 +30,31 @@ test('admin access dashboard', async ({ page }) => {
   await expect(page).toHaveURL(URL);
   await page.getByText('Dashboard').click();
   await page.getByRole('button', { name: 'List Bikes' }).click();
+  await page.getByText('Logout').click();
+  await expect(page).toHaveURL(URL);
+});
+
+
+test('register test', async ({ page }) => {
+  const user: string = faker.name.firstName();
+  const email: string = faker.internet.email();
+  const password: string = faker.internet.password(20) + '1Aa**';
+
+  await page.goto(URL);
+  await page.getByText('Login').click();
+  await page.getByText('Create an account').click();
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill(user);
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Email').press('Tab');
+  await page.getByPlaceholder('Password', { exact: true }).fill(password);
+  await page.getByPlaceholder('Password', { exact: true }).press('Tab');
+  await page.getByPlaceholder('Repeat password').fill(password);
+  await page.getByRole('button', { name: 'register' }).click();
+  await page.waitForTimeout(500);
+  await page.getByText(user).click();
+  await expect(page).toHaveURL(/profile/);
   await page.getByText('Logout').click();
   await expect(page).toHaveURL(URL);
 });
